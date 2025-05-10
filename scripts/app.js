ef3ea9c5640797ea1bdaa57e94ce042fe8d870ff
sangavi <sangavisheshanka09@gmail.com>
2025-05-10 16:59:47 +0530

    Updated index.html and main.css

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
 console.log("E-Commerce Website Loaded");
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const navMenu = document.querySelector(".nav-menu");
+
+    // Toggle the navigation menu on hamburger menu click
+    hamburgerMenu.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
+});
